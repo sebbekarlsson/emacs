27a0dd1fa39542a4e825086cace9012a146125b6
ianertson <sebbekarlsson97@gmail.com>
2022-12-31 02:29:55 +0100

    adds multi-edit and multi-cursor

---
+++ custom/key-bind.el
@@ -0,0 +1,80 @@
+;(global-set-key (kbd "<M-D>") nil)
+;(global-unset-key (kbd "<M-D>"))
+
+(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-changedir)
+(evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+(evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+(evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+(evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+(evil-define-key 'normal neotree-mode-map (kbd "c") 'neotree-create-node)
+(evil-define-key 'normal neotree-mode-map (kbd "D") 'neotree-delete-node)
+(evil-define-key 'normal neotree-mode-map (kbd "r") 'neotree-rename-node)
+(evil-define-key 'normal neotree-mode-map (kbd "yy") 'neotree-copy-node)
+
+;(global-set-key (kbd "<M-n>") 'evil-mc-make-and-goto-next-match)
+(global-set-key (kbd "<M-left>") 'windmove-left)
+(global-set-key (kbd "<M-right>") 'windmove-right)
+(global-set-key (kbd "<M-up>") 'windmove-up)
+(global-set-key (kbd "<M-down>") 'windmove-down)
+
+
+(defun reload-init-file ()
+  (interactive)
+  (load-file user-init-file))
+
+(global-set-key (kbd "C-c C-l") 'reload-init-file)    ; Reload .emacs file
+
+
+ ;  (:when (modulep! :editor multiple-cursors)
+ ;     :prefix "gz"
+ ;     :nv "d" #'evil-mc-make-and-goto-next-match
+ ;     :nv "D" #'evil-mc-make-and-goto-prev-match
+ ;     :nv "s" #'evil-mc-skip-and-goto-next-match
+ ;     :nv "S" #'evil-mc-skip-and-goto-prev-match
+ ;     :nv "c" #'evil-mc-skip-and-goto-next-cursor
+ ;     :nv "C" #'evil-mc-skip-and-goto-prev-cursor
+ ;     :nv "j" #'evil-mc-make-cursor-move-next-line
+ ;     :nv "k" #'evil-mc-make-cursor-move-prev-line
+ ;     :nv "m" #'evil-mc-make-all-cursors
+ ;     :nv "n" #'evil-mc-make-and-goto-next-cursor
+ ;     :nv "N" #'evil-mc-make-and-goto-last-cursor
+ ;     :nv "p" #'evil-mc-make-and-goto-prev-cursor
+ ;     :nv "P" #'evil-mc-make-and-goto-first-cursor
+ ;     :nv "q" #'evil-mc-undo-all-cursors
+ ;     :nv "t" #'+multiple-cursors/evil-mc-toggle-cursors
+ ;     :nv "u" #'+multiple-cursors/evil-mc-undo-cursor
+ ;     :nv "z" #'+multiple-cursors/evil-mc-toggle-cursor-here
+ ;     :v  "I" #'evil-mc-make-cursor-in-visual-selection-beg
+ ;     :v  "A" #'evil-mc-make-cursor-in-visual-selection-end)
+
+
+
+
+
+;;; neotree
+;(:after neotree
+;  :map neotree-mode-map
+;  "q"     #'neotree-hide
+;  "RET"   #'neotree-enter
+;  "SPC"   #'neotree-quick-look
+;  "v"     #'neotree-enter-vertical-split
+;  "s"     #'neotree-enter-horizontal-split
+;  "c"     #'neotree-create-node
+;  "D"     #'neotree-delete-node
+;  "g"     #'neotree-refresh
+;  "r"     #'neotree-rename-node
+;  "R"     #'neotree-refresh
+;  "h"     #'+neotree/collapse-or-up
+;  "l"     #'+neotree/expand-or-open
+;  "n"     #'neotree-next-line
+;  "p"     #'neotree-previous-line
+;  "N"     #'neotree-select-next-sibling-node
+;  "P"     #'neotree-select-previous-sibling-node)
+;
+
+;(:when (modulep! :ui neotree)
+;        :desc "Project sidebar"              "p" #'+neotree/open
+;        :desc "Find file in project sidebar" "P" #'+neotree/find-this-file)
--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (add-to-list 'load-path "~/.emacs.d/custom")
 (require 'glms-mode)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,7 +18,7 @@
  '(custom-safe-themes
    '("72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
  '(package-selected-packages
-   '(company-glsl lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode)))
+   '(evil-multiedit lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,21 +36,11 @@
 (require 'evil)
 (evil-mode 1)
 
-(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
-(evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-changedir)
-(evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
-(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
-(evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
-(evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
-(evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
-(evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
-(evil-define-key 'normal neotree-mode-map (kbd "c") 'neotree-create-node)
+(require 'evil-mc)
+(global-evil-mc-mode  1)
 
-(global-set-key (kbd "<M-left>") 'windmove-left)
-(global-set-key (kbd "<M-right>") 'windmove-right)
-(global-set-key (kbd "<M-up>") 'windmove-up)
-(global-set-key (kbd "<M-down>") 'windmove-down)
-
+(require 'evil-multiedit)
+(evil-multiedit-default-keybinds)
 
 
 (setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
@@ -100,14 +91,4 @@
 (set-face-foreground 'vertical-border
   (face-background 'vertical-border nil t))
 
-
-(global-set-key [?\C-\+] 'text-scale-increase)
-(global-set-key [?\C-\=] 'text-scale-increase)
-(global-set-key [?\C-\-] 'text-scale-decrease)
-
-
-(defun reload-init-file ()
-    (interactive)
-      (load-file user-init-file))
-
-(global-set-key (kbd "C-c C-l") 'reload-init-file)    ; Reload .emacs file
+(load "key-bind")
